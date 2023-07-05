1c440d6ce0fcc1dce414271dd7651d4f787b575e
4real <100953533+strainxx@users.noreply.github.com>
2023-07-05 10:30:09 +0300

    Add files via upload

---
+++ DataMosher/Form1.Designer.cs
@@ -0,0 +1,249 @@
+﻿namespace DataMosher
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.button1 = new System.Windows.Forms.Button();
+            this.origFileLabel = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.radioButton1 = new System.Windows.Forms.RadioButton();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.radioButton2 = new System.Windows.Forms.RadioButton();
+            this.maxbyte = new System.Windows.Forms.NumericUpDown();
+            this.label4 = new System.Windows.Forms.Label();
+            this.maxarr = new System.Windows.Forms.NumericUpDown();
+            this.label5 = new System.Windows.Forms.Label();
+            this.button2 = new System.Windows.Forms.Button();
+            this.groupBox1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.maxbyte)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.maxarr)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            this.openFileDialog1.Filter = "Image Files|*.jpg;*.jpeg;*.png";
+            this.openFileDialog1.ShowReadOnly = true;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(12, 43);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(180, 41);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Open Image to datamosh";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // origFileLabel
+            // 
+            this.origFileLabel.BackColor = System.Drawing.SystemColors.ControlDark;
+            this.origFileLabel.Font = new System.Drawing.Font("Unispace", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.origFileLabel.Location = new System.Drawing.Point(208, 48);
+            this.origFileLabel.Name = "origFileLabel";
+            this.origFileLabel.Size = new System.Drawing.Size(263, 30);
+            this.origFileLabel.TabIndex = 1;
+            this.origFileLabel.Text = "Path/to/image";
+            this.origFileLabel.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(211, 182);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(53, 16);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Options";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(65, 235);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(52, 16);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Method";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(350, 234);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(49, 16);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "Values";
+            // 
+            // radioButton1
+            // 
+            this.radioButton1.AutoSize = true;
+            this.radioButton1.Checked = true;
+            this.radioButton1.Location = new System.Drawing.Point(6, 21);
+            this.radioButton1.Name = "radioButton1";
+            this.radioButton1.Size = new System.Drawing.Size(116, 20);
+            this.radioButton1.TabIndex = 5;
+            this.radioButton1.TabStop = true;
+            this.radioButton1.Text = "Random bytes";
+            this.radioButton1.UseVisualStyleBackColor = true;
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.radioButton2);
+            this.groupBox1.Controls.Add(this.radioButton1);
+            this.groupBox1.Location = new System.Drawing.Point(32, 254);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(137, 110);
+            this.groupBox1.TabIndex = 6;
+            this.groupBox1.TabStop = false;
+            // 
+            // radioButton2
+            // 
+            this.radioButton2.AutoSize = true;
+            this.radioButton2.Enabled = false;
+            this.radioButton2.Location = new System.Drawing.Point(7, 48);
+            this.radioButton2.Name = "radioButton2";
+            this.radioButton2.Size = new System.Drawing.Size(118, 20);
+            this.radioButton2.TabIndex = 6;
+            this.radioButton2.TabStop = true;
+            this.radioButton2.Text = "Audio to image";
+            this.radioButton2.UseVisualStyleBackColor = true;
+            // 
+            // maxbyte
+            // 
+            this.maxbyte.Location = new System.Drawing.Point(351, 285);
+            this.maxbyte.Maximum = new decimal(new int[] {
+            255,
+            0,
+            0,
+            0});
+            this.maxbyte.Name = "maxbyte";
+            this.maxbyte.Size = new System.Drawing.Size(120, 22);
+            this.maxbyte.TabIndex = 7;
+            this.maxbyte.Value = new decimal(new int[] {
+            255,
+            0,
+            0,
+            0});
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(361, 266);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(97, 16);
+            this.label4.TabIndex = 8;
+            this.label4.Text = "Max byte value";
+            // 
+            // maxarr
+            // 
+            this.maxarr.Location = new System.Drawing.Point(351, 342);
+            this.maxarr.Maximum = new decimal(new int[] {
+            99999,
+            0,
+            0,
+            0});
+            this.maxarr.Name = "maxarr";
+            this.maxarr.Size = new System.Drawing.Size(120, 22);
+            this.maxarr.TabIndex = 9;
+            this.maxarr.ThousandsSeparator = true;
+            this.maxarr.Value = new decimal(new int[] {
+            15000,
+            0,
+            0,
+            0});
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(357, 323);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(101, 16);
+            this.label5.TabIndex = 10;
+            this.label5.Text = "Max array index";
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(168, 406);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(147, 43);
+            this.button2.TabIndex = 11;
+            this.button2.Text = "Datamosh!";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(483, 520);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.maxarr);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.maxbyte);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.origFileLabel);
+            this.Controls.Add(this.button1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.Text = "Image Datamosher";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.maxbyte)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.maxarr)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label origFileLabel;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.RadioButton radioButton1;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.RadioButton radioButton2;
+        private System.Windows.Forms.NumericUpDown maxbyte;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.NumericUpDown maxarr;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Button button2;
+    }
+}
+
---
+++ DataMosher/Form1.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace DataMosher
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog origDialog = new OpenFileDialog();
+            origDialog.Filter = "Images|*.jpg;*.jpeg;*.png;...";
+            if (origDialog.ShowDialog() == DialogResult.OK)
+            {
+                MessageBox.Show("Loaded image", "Datamosher", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                origFileLabel.Text = origDialog.FileName;
+            }
+        }
+
+        void datamoshImg(string imgpath, string output, int maxByte, int maxArray)
+        {
+            byte[] byteData = File.ReadAllBytes(imgpath);
+            Random r = new Random();
+            for (int i = 0; i < 2000000; i++)
+            {
+                byteData[r.Next(1000, maxArray)] = (byte)r.Next(maxByte);
+            }
+            File.WriteAllBytes(output, byteData);
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            string ext = Path.GetExtension(origFileLabel.Text);
+            //MessageBox.Show(ext);
+            datamoshImg(origFileLabel.Text, Environment.GetEnvironmentVariable("USERPROFILE") + @"\" + "Downloads\\datamoshed" + ext, (int)maxbyte.Value, (int)maxarr.Value);
+        }
+    }
+}
